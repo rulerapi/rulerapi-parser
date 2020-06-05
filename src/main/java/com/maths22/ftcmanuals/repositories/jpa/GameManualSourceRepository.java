5b4b02b720e17a869da35d51ac757a5d9719c965
Travis Weir <tweirtx@gmail.com>
2020-06-05 18:17:24 -0500

    Delete some more things

--- src/main/java/com/maths22/ftcmanuals/repositories/jpa/GameManualSourceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.maths22.ftcmanuals.repositories.jpa;
-
-import com.maths22.ftcmanuals.models.GameManualSource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameManualSourceRepository extends JpaRepository<GameManualSource, Long> {
-    // Relevant queries will be added
-}
--- src/main/java/com/maths22/ftcmanuals/repositories/jpa/VBulletinForumSourceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.maths22.ftcmanuals.repositories.jpa;
-
-import com.maths22.ftcmanuals.models.VBulletinForumSource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VBulletinForumSourceRepository extends JpaRepository<VBulletinForumSource, Long> {
-    // Relevant queries will be added
-}
